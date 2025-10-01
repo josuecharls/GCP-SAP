1d0ff71403221a06e435f2d085a85d9bd3ab8865
josuecharls <josuecharls1@gmail.com>
2025-10-01 11:52:11 -0500

    Primer commit con README

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodimac.SAP.GCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
